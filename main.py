f6d1b2ae4b91503f8739cd06b6f67fd9e156bf6d
Leandro Silva <leanoaks@gmail.com>
2023-01-21 21:40:35 -0300

    removed the non_repeating list

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def match_letter(l: str)-> str:
     match l:
         case ('à' | 'á' | 'ã' | 'â' | 'a') : return 'a'
         case ('é' | 'ê' | 'e')             : return 'e'
-        case ('í' | 'í')                    : return 'i'
-        case ('ó' | 'ô' | 'õ' | 'o')        : return 'o'
-        case ('ú' | 'ü' | 'u')              : return 'u'
-        case ('ç' | 'c')                    : return 'c'
+        case ('í' | 'í')                   : return 'i'
+        case ('ó' | 'ô' | 'õ' | 'o')       : return 'o'
+        case ('ú' | 'ü' | 'u')             : return 'u'
+        case ('ç' | 'c')                   : return 'c'
         case _                             : return l
 
 def main():
     words = get_words()
-    non_repeating = []
+    longests = [words[0]]
     for i in words:
         letters = [
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -33,18 +33,12 @@ def main():
             if char in letters:
                 letters.remove(char)
             else:
-                print(i+' is desqualified')
                 doesnt_repeat = False
         if doesnt_repeat:
-            non_repeating.append(i)
-    print(str(len(non_repeating))+' remain')
-    longests = [non_repeating[0]]
-    for i in non_repeating:
-        if len(i) > len(longests[0]):
-            longests = [i]
-        elif len(i) == len(longests[0]):
-            longests.append(i)
-
+            if len(i) > len(longests[0]):
+                longests = [i]
+            elif len(i) == len(longests[0]):
+                longests.append(i)
     print(longests)
 
 if __name__ == "__main__":
